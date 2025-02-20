5e302d3bb89fc43791c62395947d1c87877f087a
QA-ntum <fakhretdinov1992@gmail.com>
2025-02-20 23:00:07 +0500

    Update picture_testing_without_pytest.py

--- python_project/picture_testing_without_pytest.py
+++ python_project/picture_testing_without_pytest.py
@@ -10,8 +10,12 @@ max_width, max_height = 475, 475
 def test_check_image_size(min_width, min_height, max_width, max_height):
     global start_picture
     while True:
+        if start_picture < 100:
+            formatted_value = f"{start_picture:03}"
+        else:
+            formatted_value = str(start_picture)
         try:
-            url = f'https://storage.yandexcloud.net/qastudio/pokemon_battle/pokemons/{start_picture}.png'
+            url = f'https://storage.yandexcloud.net/qastudio/pokemon_battle/pokemons/{formatted_value}.png'
             response = requests.get(url)
             response.raise_for_status()
 
@@ -21,12 +25,10 @@ def test_check_image_size(min_width, min_height, max_width, max_height):
                 print(f'Изображение {url} корректно')
             else:
                 print(f'Изображение {url} не корректно| текущий размер {width}x{height}')
-
             start_picture = start_picture + 1
 
         except Exception as e:
-            print(f'Вероятно изображения закончились: {e}') # Ожидаю что 404 получаю по причине отсутствия картинки
-            break # и прерываю цикл
+            print(f'Вероятно изображения закончились: {e}')
+            break
 
 test_check_image_size(min_width, min_height, max_width, max_height)
-
